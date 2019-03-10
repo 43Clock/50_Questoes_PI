498ee702844004e483dd0d5aaefa06ef821e4926
43Clock <48367937+43Clock@users.noreply.github.com>
2019-03-10 11:39:32 +0000

    Update p35.c

--- p35.c
+++ p35.c
@@ -2,7 +2,7 @@
 
 int comunsOrd (int a[],int na,int b[],int nb) {
 	int i,j = 0,acc = 0;
-	for (i = 0;i<na;) {
+	for (i = 0;i<na && j<nb;) {
 		if (a[i]<b[j]) i++;
 		if (a[i]>b[j]) j++;
 		if (a[i] == b[j]) {
